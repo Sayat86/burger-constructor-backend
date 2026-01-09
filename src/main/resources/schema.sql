87498359d75b362e8667c749c5f47305fb58c031
Sayat86 <s.beysembayev@bk.ru>
2026-01-09 19:44:29 +0600

    correct schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,37 @@
+
 CREATE TABLE users
 (
-    id    BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name  VARCHAR(255) NOT NULL
+    id            UUID PRIMARY KEY,
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    name          VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+
+    created_at    TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_at    TIMESTAMP    NOT NULL DEFAULT now()
 );
+
 
 CREATE TABLE ingredients
 (
-    id            BIGSERIAL PRIMARY KEY,
+    id            UUID PRIMARY KEY,
     name          VARCHAR(255)   NOT NULL,
     type          VARCHAR(100)   NOT NULL,
 
-    proteins      NUMERIC(6, 2)  NOT NULL,
-    fat           NUMERIC(6, 2)  NOT NULL,
-    carbohydrates NUMERIC(6, 2)  NOT NULL,
+    proteins      INTEGER  NOT NULL,
+    fat           INTEGER  NOT NULL,
+    carbohydrates INTEGER  NOT NULL,
     calories      INTEGER        NOT NULL,
 
     price         NUMERIC(10, 2) NOT NULL,
 
-    image         TEXT,
-    image_large   TEXT,
-    image_mobile  TEXT
+    image         VARCHAR(1000) NOT NULL,
+    image_large   VARCHAR(1000) NOT NULL,
+    image_mobile  VARCHAR(1000) NOT NULL
 );
 
 CREATE TABLE orders
 (
-    id         BIGSERIAL PRIMARY KEY,
+    id         UUID PRIMARY KEY,
     user_id    BIGINT      REFERENCES users (id) ON DELETE SET NULL,
 
     status     VARCHAR(50) NOT NULL,
@@ -39,8 +45,9 @@ CREATE TABLE orders
 
 CREATE TABLE order_ingredients
 (
-    order_id      BIGINT REFERENCES orders (id) ON DELETE CASCADE,
-    ingredient_id BIGINT REFERENCES ingredients (id) ON DELETE RESTRICT,
+    order_id      UUID REFERENCES orders (id) ON DELETE CASCADE,
+    ingredient_id UUID REFERENCES ingredients (id) ON DELETE RESTRICT,
+    quantity      INTEGER NOT NULL,
 
     PRIMARY KEY (order_id, ingredient_id)
 );
